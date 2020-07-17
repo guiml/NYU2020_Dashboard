a960d45887d81476db749db6cc62481502819f73
guiml <guiml@NoteGui.localdomain>
2020-07-17 16:19:29 -0400

    diff map test

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import pandas as pd
 import plotly.graph_objs as go
 import plotly.express as px
 from dash.dependencies import Input, Output
+import json
+from urllib.request import urlopen
 
 gentr = pd.read_csv('data/gentrification.csv')
 mapbox_access_token = 'pk.eyJ1IjoiZ3VqaW1sIiwiYSI6ImNrY21pamg4dDAxZmEyc2xjdzZtNjY3YTIifQ.PKI5m9ZE6OTgj_ZhBCHyiw'
@@ -15,17 +17,23 @@ app = dash.Dash(__name__)
 app.config.suppress_callback_exceptions = True
 server = app.server
 
-#building_sales = pd.read_csv('data/sales.csv')
-#building_sales = building_sales[building_sales['SALE PRICE'] != 0]
-#building_sales['SALE DATE'] = pd.to_datetime(building_sales['SALE DATE'])
-#time_price = building_sales[['SALE DATE','SALE PRICE']]
-#df_time = time_price.set_index(['SALE DATE'])
-#time_price_means = df_time.resample('M').mean()
-#time_price_means['Date'] = time_price_means.index
-
 time_price_means = pd.read_csv('data/timepricemeans.csv')
 fig1 = px.line(time_price_means, x='Date', y='SALE PRICE')
 fig1['layout'].update({'height': 200})
+
+
+
+with urlopen('https://gist.githubusercontent.com/akash-y/eec842afd41ca3090ee402a235faeb37/raw/1e93801cd084e00c4b49a90582e7578689787354/test.geojson') as response:
+    tracts = json.load(response)
+with urlopen('https://gist.githubusercontent.com/akash-y/981a07f9924b2aec750ef05d8f0ded59/raw/c5cc1bbdd2a0a63b5ca1d70eaed3daef0cab623f/ny_gentrification_2018.geojson') as response:
+    ny_map = json.load(response)
+with urlopen('https://gist.githubusercontent.com/akash-y/6aa5d1fe4bfecda6b2ba7bd4b918e209/raw/04134738f6cce463f787f8a20dc2a1639e15f64c/ny_zip.geojson') as response:
+    ny_zip = json.load(response)
+
+    
+gentrification_2018_df = pd.read_csv('https://gist.githubusercontent.com/akash-y/aa7e340b02ac6f8cc78b3f5698bb95b8/raw/87deac83e18a099ac02ca215e3af354c7581f4eb/redhook_predictions_2018.csv')
+gentrification_2018_ny = pd.read_csv('https://gist.githubusercontent.com/akash-y/0e6a14fa614aabb16b5b35a5273e44ca/raw/ee7aace5cf795aa005cc563c02be97552633b7da/ny_gentrification_2018.csv')
+evictions_df = pd.read_csv('https://gist.githubusercontent.com/akash-y/e0ffea12dde217ec49546ffa66461ce5/raw/143edbf60b34e34139545cba079124ed01833652/ny_evictions.csv')
 
 
 def get_options(df_menu):
@@ -72,12 +80,17 @@ app.layout = html.Div(
 def update_figure(selected):
     selectedval = selected
     filtered_gentr = gentr[gentr.ntacode == selected]
-    fig = go.Figure(go.Scattermapbox(lat=filtered_gentr['Lat'],lon=filtered_gentr['Lon'],mode='markers',marker=go.scattermapbox.Marker(color=filtered_gentr['gentrifica'])))
-    fig.update_layout(mapbox=dict(accesstoken=mapbox_access_token, center=dict(lon=-73.99,lat=40.72), zoom=10))
-    fig.update_layout(margin={"r":20,"t":20,"l":20,"b":20})
-    fig.update_layout({'height': 200, 'width': 800})
-    fig.update_layout(transition_duration=500)
-    return fig
+    commercial_evictions = px.choropleth_mapbox(evictions_df, geojson=ny_zip,locations = 'MODZCTA' ,featureidkey="properties.MODZCTA",color='commercial_pctl_score',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 100),
+                           mapbox_style="carto-positron",
+                           zoom=10, center = {"lat": 40.724576, "lon": -73.916812},
+                           opacity=0.5,
+                           labels={'prediction':'Commercial Evictions Percentile Score'}
+                          )
+    commercial_evictions.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    commercial_evictions.update_layout({'height': 200, 'width': 800})
+    return commercial_evictions
 
 
 
